2ab5f67288c513e98d05ba1f562363534e0619fd
YEONSEO <yeonseochoi0717@gmail.com>
2024-10-26 00:35:11 +0900

    feat: 적절하지 않은 횟수 입력 값 에러 처리

--- src/validation.js
+++ src/validation.js
@@ -1,16 +1,44 @@
 class Validation {
     constructor() {}
 
+    static isNumber(value) {
+        if(isNaN(value)){
+            const errMessage = `[ERROR] 시도 횟수는 숫자를 입력해주세요(Not Number: ${value})`;
+            throw new Error(errMessage);
+        }
+
+        return Number(value);
+    }
+
+    static attemptsValidate(attempts) {
+        attempts = Validation.isNumber(attempts)
+
+        if (!Number.isInteger(attempts)) {
+            const errMessage = `[ERROR] 시도 횟수는 정수를 입력해주세요(Not Integer: ${attempts})`;
+            throw new Error(errMessage);
+        }
+
+        if (attempts < 0) {
+            const errMessage = `[ERROR] 시도 횟수는 양수를 입력해주세요(Negative Number: ${attempts})`;
+            throw new Error(errMessage);
+        }
+
+    }
+
     static inputValidate(input, type) {
         if (!input) {
             const errMessage = `[ERROR] ${type} 값을 입력해주세요(Empty Input)`;
             throw new Error(errMessage);
         }
+
+        if (type == '시도 횟수') {
+            Validation.attemptsValidate(input);
+        }
     }
 
     static nameValidate(name) {
         if (name.length > 5) {
-            const errMessage = `[ERROR] 자동차 이름을 5자 이하로 작성해주세요`;
+            const errMessage = `[ERROR] 자동차 이름을 5자 이하로 작성해주세요(Long Name: ${name})`;
             throw new Error(errMessage);
         }
     }
